1c5cafb12f9f0d1cd1073e4e6caa46d6f979247c
ChitraKrishna <98392029+ChitraKrishna@users.noreply.github.com>
2022-02-22 19:19:39 +0530

    Add project files.

---
+++ Models/Dbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication22.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> Options) : base(Options)
+        { }
+        public DbSet<StudentDbContext> Student { get; set; }
+
+    }  
+}
---
+++ Models/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication22.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mobaile { get; set; }
+        public string Collage { get; set; }
+    }
+}
